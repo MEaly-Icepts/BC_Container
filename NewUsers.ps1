f1057c9df857b2d76f94414c84a569a3d5d1d7c4
Mathew Ealy <Mealy@icepts.com>
2022-07-11 10:40:12 -0400

    Added NewUsers,ps1

---
+++ NewUsers.ps1
@@ -0,0 +1,18 @@
+#>
+if (Get-Module -ListAvailable -Name BcContainerHelper) {
+    Import-Module BcContainerHelper
+} 
+else {
+    Get-Module BcContainerHelper
+}
+
+$users = Import-CSV -path "C:\Folder\User.csv"
+$Container = "ContainerName"
+
+foreach ($user in $users)
+{
+   $credential = $user.'Credential'
+   $PermissionSet = $User.'PermissionSet'
+  
+New-BcContainerBcUser -containerName $Container -Credential $credential -assignPremiumPlan -PermissonsetId $PermissionSet
+}
