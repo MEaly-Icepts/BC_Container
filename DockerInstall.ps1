f43080111dcc927ab9804336ec81c5da60fb0f61
Mathew Ealy <Mealy@icepts.com>
2022-07-22 11:25:10 -0400

    Build .5 of Docker install file

---
+++ DockerInstall.ps1
@@ -0,0 +1,35 @@
+<#
+First we test if Choclatey is installed and if not install it. 
+#>
+
+$testchoco = powershell choco -v
+if(-not($testchoco)){
+    Write-Output "Seems Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else{
+    Write-Output "Chocolatey Version $testchoco is already installed"
+}
+
+<#
+Test if Hyper-V is enabled. Enable if not
+#>
+
+$hyperv = Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V -Online
+if($hyperv.State -eq "Enabled"){
+    Write-Output "Hyper-V enabled"
+}
+else{
+    Write-Output "Enabling Hyper-V"
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+}
+
+<#
+Install Docker desktop application using chocolaty & add user to the docker group
+#>
+Write-Output "Installing Docker Desktop"
+choco install docker-desktop
+#Determine logged in user add to docker group
+Write-Output "Adding user to Docker group"
+$User = $env:UserName
+Add-LocalGroupMember -Group Docker-users -Member $User -Confirm
