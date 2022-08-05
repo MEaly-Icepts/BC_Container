7b529d3c6fe57445945a6107c564e2b4e68eb2fa
Mathew Ealy <Mealy@icepts.com>
2022-08-05 11:13:30 -0400

    fixed new user header & comments

--- NewUsers.ps1
+++ NewUsers.ps1
@@ -1,13 +1,32 @@
+<#!
+.SYNOPSIS
+	Add users to Dynamics BC container.
+.DESCRIPTION
+	This script will import new users into Dynamics BC container
+.NOTES
+	File Name: NewUsers.ps1
+	Author: Mathew Ealy
+	Requires Powershell 5.0
+.LINK
+	https://github.com/MEaly58
+#>
+
+#Check if BcContainerHelper module is installed & load it. Download & load if not yet installed. 
 if (Get-Module -ListAvailable -Name BcContainerHelper) {
+    Write-Output "Loading BcContainerHelper"
     Import-Module BcContainerHelper
 } 
 else {
-    Get-Module BcContainerHelper
+    Write-Output "BcContainerHelper Missing, intalling now"
+    Install-Module -Name BcContainerHelper
+    Import-Module BcContainerHelper
 }
 
+#variables
 $users = Import-CSV -path "C:\Folder\User.csv"
 $Container = "ContainerName"
 
+#Actions
 foreach ($user in $users)
 {
    $credential = $user.'Credential'
