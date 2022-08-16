50779fe906121d2c85cb8d87c3c36939de01b2ad
Mathew Ealy <Mealy@icepts.com>
2022-08-16 15:47:52 -0400

    updating comments & gitignore

--- DockerInstallV2.ps1
+++ DockerInstallV2.ps1
@@ -14,7 +14,7 @@ Install Docker
 Write-Output "Installing Docker Desktop"
 Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
 .\install-docker-ce.ps1
-#Removed test for docker install as it was not a good solution. 
+
 
 <#
 Determine logged in user add to docker group - requires reboot to pick up new group memebership
@@ -25,7 +25,7 @@ Add-LocalGroupMember -Group Docker-users -Member $User -Confirm
 
 <#
 Test if Hyper-V is enabled. Enable if not ** requires a restart if enabling Hyper-V
-#>
+
 $hyperv = Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V -Online
 if($hyperv.State -eq "Enabled"){
     Write-Output "Hyper-V enabled"
@@ -34,3 +34,6 @@ else{
     Write-Output "Enabling Hyper-V"
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 }
+
+Commented out as it is part of the MS script being called earlier. 
+#>
