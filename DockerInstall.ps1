63a16e0059f815c29b1bf8e8ce7dff8bf28a7c05
Mathew Ealy <Mealy@icepts.com>
2022-08-16 15:54:45 -0400

    cleaed up file

--- DockerInstall.ps1
+++ DockerInstall.ps1
@@ -1,12 +1,4 @@
 #Requires -RunAsAdministrator
-$ErrorActionPreference = 'Stop';
-
-
-<#
-Initilize TLS1.2
-#>
-Write-Output "Initializing TLS 1.2"
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
 <#
 Install Docker 
@@ -14,7 +6,6 @@ Install Docker
 Write-Output "Installing Docker Desktop"
 Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
 .\install-docker-ce.ps1
-
 
 <#
 Determine logged in user add to docker group - requires reboot to pick up new group memebership
