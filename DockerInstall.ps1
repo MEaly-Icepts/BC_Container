92c1b0de0257166934850d9da1b8e47ad3aaee05
Mathew Ealy <Mealy@icepts.com>
2022-08-25 10:15:36 -0400

    fixed readability

--- DockerInstall.ps1
+++ DockerInstall.ps1
@@ -3,6 +3,7 @@
 <#
 Install Docker 
 #>
+
 Write-Output "Installing Docker Desktop"
 Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
 .\install-docker-ce.ps1
