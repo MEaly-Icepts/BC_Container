0b6e11a31924f1177473c6d56ea6df817064fc0b
Mathew Ealy <Mealy@icepts.com>
2022-09-07 09:21:50 -0400

    Removed failed Dockerinstall script

---
+++ DockerDesktopInstaller.ps1
@@ -0,0 +1,14 @@
+<#!
+.SYNOPSIS
+	Install & Configure Dockerdesktop client for MS windows systems
+.DESCRIPTION
+	install the latest version of Dockerdesktop, configure hyper-v, and add user to the Docker user group. 
+.NOTES
+	File Name: DockerDesktopinstaller.ps1
+	Author: Mathew Ealy
+	Requires Powershell 5.0
+.LINK
+	https://github.com/MEaly58
+#>
+#Requires -RunAsAdministrator
+
