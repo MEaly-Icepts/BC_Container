efcb03e81f7e0808ef8260a0cd5ca75f8ca41a8e
Mathew Ealy <Mealy@icepts.com>
2023-01-05 13:34:42 -0500

    better module management

--- ContainerBuild.ps1
+++ ContainerBuild.ps1
@@ -27,25 +27,42 @@ Write-Output "Initializing TLS 1.2"
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
 #Check if PowershellGet/NuGet is installed
-if(Get-PackageProvider -Name PowershellGet){
-	Write-Output "PowershellGet installed"
+Write-Host "Checking that PowershellGet is installed..."
+$psGetModule = Get-InstalledModule PowershellGet | Sort-Object Version | Select-Object -last 1
+if ($psGetModule)
+{
+  Write-Host "Updating PowershellGet..."
+  Update-Module PowershellGet
+  Write-Host "Getting PowershellGet module version..."
+  $psGetModule = Get-InstalledModule PowershellGet | Sort-Object Version | Select-Object -last 1
 }
-else{
-	Install-PackageProvider -Name NuGet -Force
-	Install-PackageProvider -Name PowershellGet -Force
-	Write-Output "Installing package management software"
+else
+{
+  Write-Host "Installing PowershellGet..."
+  Install-Module PowershellGet -Force
+  Write-Host "Getting PowershellGet module version..."
+  $psGetModule = Get-InstalledModule PowershellGet | Sort-Object Version | Select-Object -last 1
 }
+Write-Host "PowershellGet module version: $($psGetModule.Version)"
 
 #Check if BcContainerHelper module is installed & load it. Download & load if not yet installed. 
-if (Get-Module -ListAvailable -Name BcContainerHelper) {
-    Write-Output "Loading BcContainerHelper"
-    Import-Module BcContainerHelper
-} 
-else {
-    Write-Output "BcContainerHelper Missing, intalling now"
-    Install-Module -Name BcContainerHelper
-    Import-Module BcContainerHelper
+Write-Host "Checking that BcContainerHelper is installed..."
+$psBCCHModule = Get-InstalledModule BcContainerHelper | Sort-Object Version | Select-Object -last 1
+if ($psBCCHModule)
+{
+  Write-Host "Updating BcContainerHelper..."
+  Update-Module BcContainerHelper
+  Write-Host "Getting BcContainerHelper module version..."
+  $psBCCHModule = Get-InstalledModule BcContainerHelper | Sort-Object Version | Select-Object -last 1
 }
+else
+{
+  Write-Host "Installing BcContainerHelper..."
+  Install-Module BcContainerHelper -Force
+  Write-Host "Getting BcContainerHelper module version..."
+  $psBCCHModule = Get-InstalledModule BcContainerHelper | Sort-Object Version | Select-Object -last 1
+}
+Write-Host "BcContainerHelper module version: $($psBCCHModule.Version)"
 
 #Get latest verions of BC artifacts & mount container
 $artifactUrl = Get-BcArtifactUrl -type sandbox -country us -select Latest
