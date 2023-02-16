29918e67051bfdda6c24106fdf1d544daa433fd3
Mathew Ealy <Mealy@icepts.com>
2023-02-16 09:42:48 -0500

    removed unused code

--- PreviewBuild.ps1
+++ PreviewBuild.ps1
@@ -19,25 +19,6 @@ $CTName = "Preview"
 Write-Output "Initializing TLS 1.2"
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-#Check if PowershellGet/NuGet is installed
-Write-Host "Checking that PowershellGet is installed..."
-$psGetModule = Get-InstalledModule PowershellGet | Sort-Object Version | Select-Object -last 1
-if ($psGetModule)
-{
-  Write-Host "Updating PowershellGet..."
-  Update-Module PowershellGet
-  Write-Host "Getting PowershellGet module version..."
-  $psGetModule = Get-InstalledModule PowershellGet | Sort-Object Version | Select-Object -last 1
-}
-else
-{
-  Write-Host "Installing PowershellGet..."
-  Install-Module PowershellGet -Force
-  Write-Host "Getting PowershellGet module version..."
-  $psGetModule = Get-InstalledModule PowershellGet | Sort-Object Version | Select-Object -last 1
-}
-Write-Host "PowershellGet module version: $($psGetModule.Version)"
-
 #Check if BcContainerHelper module is installed & load it. Download & load if not yet installed. 
 Write-Host "Checking that BcContainerHelper is installed..."
 $psBCCHModule = Get-InstalledModule BcContainerHelper | Sort-Object Version | Select-Object -last 1
@@ -57,7 +38,7 @@ else
 }
 Write-Host "BcContainerHelper module version: $($psBCCHModule.Version)"
 
-
+#Download Container
 $CTName = "Preview"
 #Get latest verions of BC artifacts & mount container
 $artifactUrl = Get-BcArtifactUrl -storageAccount BcPublicPreview -country US
